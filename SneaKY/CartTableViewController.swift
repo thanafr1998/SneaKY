99919f9ce2f17d74e63872e157b0e2a45639f4e5
thanadol rungjitwaranon <thanadolrungjitwaranon@thanadols-MacBook-Pro.local>
2019-04-22 20:30:09 +0700

    stillBugBackAndAddCartTableViewController

---
+++ SneaKY/CartTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  CartTableViewController.swift
+//  SneaKY
+//
+//  Created by thanadol rungjitwaranon on 22/4/2562 BE.
+//  Copyright © 2562 thanadol rungjitwaranon. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewController: UITableViewController {
+
+    var shoesData = [[],[],[]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.shoesData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    // MARK: - Table view data source
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rentCell", for: indexPath)
+        
+        //cell.imageView?.image =
+        
+        //cell.textLabel?.text =
+        
+        return cell
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
